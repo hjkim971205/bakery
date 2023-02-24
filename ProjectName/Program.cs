edb486fda94adb360f3c04b5331006d8c6ef226d
Hyung Jin Kim <121835593+hjkim971205@users.noreply.github.com>
2023-02-24 08:03:32 -0800

    Initial commit

---
+++ ProjectName/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using ProjectName.Models;
+
+namespace ProjectName
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to the ProjectName app!");
+      Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
+      Console.WriteLine("Please enter a number:");
+    }
+
+  }
+}
